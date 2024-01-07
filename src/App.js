62e0805edbde445f878d5d3638834049bf24e129
Kadyrbaev <sadyr_9494@mail.ru>
2024-01-07 20:44:10 +0600

    input

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <h1>{res.text}</h1>
       <button onClick={()=>{dispatch(testSlice.actions.plusNum(60))}}>TEST</button>
      <button onClick={()=>{dispatch({type: "PLUS", payload: 5})}}>+</button>
+     <input type="text" />
+     <input type="text" />
     </div>
   );
 }
