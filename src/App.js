0ca933b45d50c9a69f33b9480ce4994ea5e5cb6a
Kadyrbaev <sadyr_9494@mail.ru>
2024-01-07 21:22:14 +0600

    button

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
   const res =   useSelector((state)=> state.test )
   const dispatch = useDispatch()
   console.log(res);
-
+  
   
   return (
     <div className="App">
       <h1>{res.text}</h1>
       <button onClick={()=>{dispatch(testSlice.actions.plusNum(60))}}>TEST</button>
-     <button onClick={()=>{dispatch({type: "PLUS", payload: 5})}}>+</button>
-     <input type="text" />
-     <input type="text" />
+      <button onClick={()=>{dispatch({type: "PLUS", payload: 5})}}>+</button>
+      <button>eurvheov</button>
+      <button>eurvheov-----</button>
      <h2>wufbowfbow</h2>
     </div>
   );
